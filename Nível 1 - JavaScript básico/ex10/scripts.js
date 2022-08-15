f5d01e0f80004ae037cb13f1cf4e45e5ef0a909c
Vinícius Reisch <97248458+Flyinng@users.noreply.github.com>
2022-08-15 17:18:05 -0300

    creating ex10

---
+++ Nível 1 - JavaScript básico/ex10/scripts.js
@@ -0,0 +1,17 @@
+let res = document.querySelector('p#res')
+
+function acao1() {
+    res.innerHTML += '<p> Clicou no primeiro botão <\p>'
+}
+
+function acao2() {
+    res.innerHTML += '<p> Clicou no segundo botão <\p>'
+}
+
+function acao3() {
+    res.innerHTML += '<p> Clicou no terceiro botão <\p>'
+}
+
+function acao4() {
+    res.innerHTML += '<p> Clicou no quarto botão <\p>'
+}
